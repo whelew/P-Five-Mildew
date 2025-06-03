f1c9894aeb536a51b3ba49e748065ddedc682282
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-06-03 18:18:07 +0000

    Add radio button to select healthy or powdery mildew image montage

--- streamlit_pages/page_leaf_visualiser.py
+++ streamlit_pages/page_leaf_visualiser.py
@@ -4,12 +4,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import matplotlib.image imread
+from PIL import Image
 
 import itertools
 import random
 
-OUTPUT_DIR = 'outputs/images'
 
 def page_leaf_visualiser_body():
     st.write('### Leaf Visualisation')
@@ -19,3 +18,22 @@ def page_leaf_visualiser_body():
         f'The difference between average healthy and average powdery mildew cherry leaves.'
         f'They have also asked for an image montage for each class.'
         )
+
+    option = st.radio(
+    'Choose the visualisation you would like to see:',
+    ('Average & Variability - Healthy:',
+     'Average & Variability - Powdery Mildew:',
+     'Standard Deviation - Class Comparison,'
+     'Image Montage - Healthy',
+     'Image Montage - Powdery Mildew')
+    )
+
+    if option == 'Image Montage - Healthy':
+        st.write('### Image Montage')
+        image_montage_healthy = Image.open('outputs/images/healthy_img_montage.png')
+        st.image(image_montage_healthy)   
+
+    if option == 'Image Montage - Powdery Mildew':
+        st.write('### Image Montage - Powdery Mildew') 
+        image_montage_powdery_mildew = Image.open('outputs/images/powdery_mildew_img_montage.png')
+        st.image(image_montage_powdery_mildew)   
