2011c664314d15d377081412818c80caaba9e6fb
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-06-03 20:59:23 +0000

    Removed quote marks from percentages

--- streamlit_pages/page_hypothesis.py
+++ streamlit_pages/page_hypothesis.py
@@ -12,13 +12,13 @@ def page_hypothesis():
 
     st.info("""This project explores whether a convolutional neural network (CNN) based binary classification model can 
             accurately classify cherry leaves as either healthy or ones containing powdery mildew. The model will be
-            able to achieve a prediction accuracy of at least a 97%.
+            able to achieve a prediction accuracy of at least a **97%**.
 
             **Null Hypothesis**: A CNN model trained on cherry leaf images cannot reliably distinguish between
-            healthy leaves and ones containing powdery mildew better than random chance (around '50%' accuracy).
+            healthy leaves and ones containing powdery mildew better than random chance (**50%** accuracy).
 
             **Alternative Hypothesis:** A CNN model trained on cherry leaf images can reliably distinguish between
-            healthy leaves and ones containing powdery mildew with at least a '97%' accuracy.
+            healthy leaves and ones containing powdery mildew with at least a **97%** accuracy.
         """)
     
     st.success("""The null hypothesis has been rejected in favour of the alternative hypothesis.
