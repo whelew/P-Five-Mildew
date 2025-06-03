7bdc390d68e61795d12a3e6f3bfda9af2c9e29c3
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-06-03 18:23:36 +0000

    Fixed syntax errors, if now elif, st.image now right image path

--- streamlit_pages/page_leaf_visualiser.py
+++ streamlit_pages/page_leaf_visualiser.py
@@ -13,10 +13,10 @@ import random
 def page_leaf_visualiser_body():
     st.write('### Leaf Visualisation')
     st.info(
-        f'The client has asked for a page to visualise:'
-        f'Average images and variability images for each class which is healthy or powdery mildew.'
-        f'The difference between average healthy and average powdery mildew cherry leaves.'
-        f'They have also asked for an image montage for each class.'
+        'The client has asked for a page to visualise:'
+        '- Average images and variability images for each class which is healthy or powdery mildew.'
+        '- The difference between average healthy and average powdery mildew cherry leaves.'
+        '- They have also asked for an image montage for each class.'
         )
 
     option = st.radio(
@@ -31,24 +31,24 @@ def page_leaf_visualiser_body():
     if option == 'Image Montage - Healthy':
         st.write('### Image Montage')
         image_montage_healthy = Image.open('outputs/images/healthy_img_montage.png')
-        st.image(image_montage_healthy)   
+        st.image(image_montage_healthy, use_column_width=True)   
 
-    if option == 'Image Montage - Powdery Mildew':
+    elif option == 'Image Montage - Powdery Mildew':
         st.write('### Image Montage - Powdery Mildew') 
         image_montage_powdery_mildew = Image.open('outputs/images/powdery_mildew_img_montage.png')
-        st.image(image_montage_powdery_mildew)   
+        st.image(image_montage_powdery_mildew, use_column_width=True)   
 
-    if option == 'Average & Variability - Healthy:':
+    elif option == 'Average & Variability - Healthy:':
         st.write('### Average & Variability - Healthy:') 
-        image_montage_powdery_mildew = Image.open('outputs/images/variability_within_powdery_mildew_images.png')
-        st.image(image_montage_powdery_mildew) 
+        avg_healthy = Image.open('outputs/images/variability_within_powdery_mildew_images.png')
+        st.image(avg_healthy, use_column_width=True) 
     
-    if option == 'Average & Variability - Powdery Mildew:':
+    elif option == 'Average & Variability - Powdery Mildew:':
         st.write('### Average & Variability - Powdery Mildew:') 
-        image_montage_powdery_mildew = Image.open('outputs/images/variability_within_healthy_images.png')
-        st.image(image_montage_powdery_mildew) 
+        avg_powdery_mildew = Image.open('outputs/images/variability_within_healthy_images.png')
+        st.image(avg_powdery_mildew, use_column_width=True) 
     
-    if option == 'Standard Deviation - Class Comparison':
+    elif option == 'Standard Deviation - Class Comparison':
         st.write('### Standard Deviation - Class Comparison') 
-        image_montage_powdery_mildew = Image.open('outputs/images/std_between_img_classes.png')
-        st.image(image_montage_powdery_mildew) 
+        std_class_comp = Image.open('outputs/images/std_between_img_classes.png')
+        st.image(std_class_comp, use_column_width=True) 
