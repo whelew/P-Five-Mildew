9f2c303cdd6470e1115da64242fdc95473a18ea5
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-06-03 21:44:26 +0000

    Create app.py, add code to run streamlit app

---
+++ app.py
@@ -0,0 +1,18 @@
+from streamlit_pages.multipage import MultiPage
+from streamlit_pages import (
+    page_hypothesis,
+    page_leaf_visualiser,
+    page_ml_performance,
+    page_powdery_mildew_detector,
+    page_summary
+)
+
+app = MultiPage()
+
+app.add_page('Project Summary', page_summary.page_summary)
+app.add_page('Project Hypothesis', page_hypothesis.page_hypothesis)
+app.add_page('Leaf Visualisation', page_leaf_visualiser.page_leaf_visualiser_body)
+app.add_page('Machine Learning Performance Metrics', page_ml_performance.page_ml_performance_metrics)
+app.add_page('Powdery Mildew Detector', page_powdery_mildew_detector.page_powdery_mildew_detector)
+
+app.run()
