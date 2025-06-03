469c12defab26a3be9363d8e11d3ccb0ecac1594
Lewis Wheeler <Lewisswheelerr@outlook.com>
2025-06-03 19:29:48 +0000

    Repeated structure for previous columns for cross validation and final model

--- streamlit_pages/page_ml_performance.py
+++ streamlit_pages/page_ml_performance.py
@@ -35,6 +35,12 @@ def page_ml_performance_metrics():
         else:
             st.error(f'Image path not found {image_path}')
 
+    def check_csv_path_exists(csv_path):
+        if os.path.exists(csv_path):
+            csv_results_df = pd.read_csv(csv_path)
+            st.dataframe(csv_results_df)
+        else:
+            st.error(f'CSV file can not be found {csv_path}')
 
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -77,3 +83,47 @@ def page_ml_performance_metrics():
     st.write('---')
 
     st.subheader('Cross Validation Search')
+
+    col7, col8, col9 = st.columns(3)
+    with col7:
+        st.info(""" After finding the best hyper parameters I created a model using them
+                as the values for my next model. This model then performed a cross validation check
+                using the stratified k-fold method. The results showed that the model was 
+                generalising well and there were no signs of fold dependent overfitting.
+                Here are the results from the cross validation search, it achieved an average accuracy 
+                of 99.37%. 
+            """)
+        cross_val_csv_path = 'outputs/logs/cross_validation_results.csv'
+        check_csv_path_exists(cross_val_csv_path)
+    with col8:
+        st.info('Here is a classification report detailing the performance of the model.')
+        cross_val_model_rep = 'outputs/logs/class_rep_for_cross_val.txt'
+        check_txt_path_exists(cross_val_model_rep)
+    with col9:
+        st.info('Here is a confusion matrix detailing the performance of the model.')
+        cross_val_model_conf = 'outputs/images/cross_val_conf_matrix.png'
+        check_image_path_exists(cross_val_model_conf, caption='Cross Validaiton Model Confusion Matrix')
+        
+    st.write('---')
+
+    st.subheader('Final Model History')
+
+    col10, col11, col12 = st.columns(3)
+    with col10:
+        st.info(""" After checking performance metrics throughout the final model was created using
+                the best hyperparameters along with the knowledge it is not over fitting. The final
+                model was trained using a full train dataset which is a combination dataset of the original
+                train and validaiton sets.  
+            """)
+        final_model_csv_path = 'outputs/logs/final_model_history.csv'
+        check_csv_path_exists(final_model_csv_path)
+        final_model_img_path = 'outputs/images/final_model_history_plot'
+        check_image_path_exists(final_model_img_path)
+    with col11:
+        st.info('Here is a classification report detailing the performance of the final model.')
+        final_model_class_rep = 'outputs/logs/final_model_class_report.txt'
+        check_txt_path_exists( final_model_class_rep)
+    with col12:
+        st.info('Here is a confusion matrix detailing the performance of the model.')
+        fianl_model_conf_matrix = 'outputs/images/final_model_conf_matrix.png'
+        check_image_path_exists(fianl_model_conf_matrix, caption='Final Model Confusion Matrix')
